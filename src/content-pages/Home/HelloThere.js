5e0a8ff20a53d49716f24a9b2fb2cdd9af5e4692
Sergio Lazaro Mondargo <kingsergiolm17@gmail.com>
2022-12-22 12:01:07 -0500

    Initial commit

---
+++ src/content-pages/Home/HelloThere.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+export default function HelloThere() {
+  return (
+    <>
+      <section>Hello There OK</section>
+
+      <section data-markdown style={{ height: "100vh" }}>
+        <div
+          // data-template
+          style={{ overflowY: "auto", width: "100%", height: "100%" }}
+        >
+          reveal.js da sd enables you to create beautiful interactive slide
+          decks using HTML. This presentation will show you examples of what it
+          can do. reveal.js enables you to create beautiful interactive slide
+          decks using HTML. This presentation will show you examples of what it
+          can do. reveal.js enables you to create beautiful interactive slide
+          decks using HTML. This presentation will show you examples of what it
+          can do. vreveal.js enables you to create beautiful interactive slide
+          decks using HTML. This presentation will show you examples of what it
+          can do. reveal.js enables you to create beautiful interactive slide
+          decks using HTML. This presentation will show you examples of what it
+          can do. vreveal.js enables you to create beautiful interactive slide
+          decks using HTML. This presentation will show you examples of what it
+          can do. reveal.js enables you to create beautiful interactive slide
+          decks using HTML. This presentation will show you examples of what it
+          decks using HTML. This presentation will show you examples of what it
+          can do. vreveal.js enables you to create beautiful interactive slide
+          decks using HTML. This presentation will show you examples of what it
+          can do. reveal.js enables you to create beautiful interactive slide
+          decks using HTML. This presentation will show you examples of what it
+          decks using HTML. This presentation will show you examples of what it
+          can do. vreveal.js enables you to create beautiful interactive slide
+          decks using HTML. This presentation will show you examples of what it
+          can do. reveal.js enables you to create beautiful interactive slide
+          decks using HTML. This presentation will show you examples of what it
+          decks using HTML. This presentation will show you examples of what it
+          can do. vreveal.js enables you to create beautiful interactive slide
+          decks using HTML. This presentation will show you examples of what it
+          can do. reveal.js enables you to create beautiful interactive slide
+          decks using HTML. This presentation will show you examples of what it
+          decks using HTML. This presentation will show you examples of what it
+          can do. vreveal.js enables you to create beautiful interactive slide
+          decks using HTML. This presentation will show you examples of what it
+          can do. reveal.js enables you to create beautiful interactive slide
+          decks using HTML. This presentation will show you examples of what it
+          can do.
+        </div>
+      </section>
+    </>
+  );
+}
